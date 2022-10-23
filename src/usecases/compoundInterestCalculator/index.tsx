0e7a5f25581cbeb42aa692897d77423b576929ab
Aman-201 <81082654+Aman-201@users.noreply.github.com>
2022-10-24 00:59:54 +0530

    compound interest calculator done

--- src/usecases/compoundInterestCalculator/index.tsx
+++ src/usecases/compoundInterestCalculator/index.tsx
@@ -13,16 +13,54 @@ const CompoundInterestCalculator: React.FC = () => {
   const [principal, setPrincipal] = useState<string>('0');
   const [time, setTime] = useState<string>('0');
   const [interest, setInterest] = useState<string>('0');
+  const [frequency, setFrequency] = useState<string>('0');
   const [compound_amt, setCompound_amt] = useState<number>();
+  const [result, setResult] = useState('');
+  const [error, setError] = useState('');
   const handleClick = () => {
     var t = parseInt(time);
     var p = parseInt(principal);
     var i = parseFloat(interest);
-    var cp_int = p * (1 + i);
-    cp_int = Math.pow(cp_int, t);
-    setCompound_amt(cp_int);
-    console.log(compound_amt);
+    var f=parseInt(frequency);
+    if (
+      t === 0 &&
+      p === 0 &&
+      i === 0 &&
+      f === 0
+    ) {
+      setError(
+        `Principal Amount,  Time Duration, Interest Frequency and Interest Rate cannot be 0.`
+      );
+      return;
+    }
+    if (p === 0) {
+      setError(`Principal Amount cannot be 0.`);
+      return;
+    }
+    if (t === 0) {
+      setError(`Time Duration cannot be 0.`);
+      return;
+    }
+    if (i === 0) {
+      setError(`Interest Rate cannot be 0.`);
+      return;
+    }
 
+    if (f === 0) {
+      setError(`Interest Frequency cannot be 0.`);
+      return;
+    }
+    i/=100;
+    
+    var power=(f*t);
+   
+    var cp_int =p* Math.pow((1 + i/f), power);
+
+    setCompound_amt(cp_int-p);
+    setError('');
+    setResult(`Compound Interest will be ${cp_int-p} , with net worth ${cp_int}.`);
+    console.log(compound_amt);
+    
     return;
   };
   return (
@@ -55,18 +93,46 @@ const CompoundInterestCalculator: React.FC = () => {
             setInterest(e.target.value);
           }}
         />
+          <TextField
+          id="frequency"
+          label="Compound Interest Frequency Anually"
+          value={frequency}
+          variant="outlined"
+          onChange={(e) => {
+            setFrequency(e.target.value);
+          }}
+        />
       </Stack>
-      <Button
-        variant="outlined"
-        type="submit"
-        sx={{
-          mt: 1,
-          boxShadow: 2,
-        }}
-        onClick={handleClick}
-      >
-        Calculate
-      </Button>
+      <Button variant="outlined" type="submit" sx={{
+        mt:1,boxShadow:2
+      }} onClick={handleClick}>Calculate</Button>
+    
+    {result.length !== 0 && (
+        <Typography
+          variant="h6"
+          sx={{
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
+        >
+          {result}
+        </Typography>
+      )}
+
+      {error.length !== 0 && (
+        <Typography
+          variant="h6"
+          sx={{
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
+        //  style={{ color: theme.palette.error.main }}
+        >
+          {error}
+        </Typography>
+      )}
+    
+    
     </Stack>
   );
 };
