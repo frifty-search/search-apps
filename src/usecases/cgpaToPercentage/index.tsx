977a4183a5f7feb64bf1cfca7053e7812e30dc63
Aman-201 <81082654+Aman-201@users.noreply.github.com>
2022-10-25 14:39:33 +0530

    feat: add usecase 28

--- src/usecases/cgpaToPercentage/index.tsx
+++ src/usecases/cgpaToPercentage/index.tsx
@@ -1,7 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Button,
+  Stack,
+  TextField,
+  Typography,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+} from '@mui/material';
 
 const CgpaToPercentage: React.FC = () => {
-  return <></>;
+  const [cgpa, setCgpa] = useState<string>('0');
+  const [percentage, setPercentage] = useState<string>('0');
+  const [result, setResult] = useState('');
+  const [error, setError] = useState('');
+  const handleClick = () => {
+    if (cgpa === '0') {
+      setError('Enter a valid CGPA');
+      return;
+    }
+    var current_cg = parseFloat(cgpa);
+    var percent = current_cg * 9.5;
+    setPercentage(percentage);
+    setError('');
+    setResult(`${current_cg} CGPA will be ${percent} %`);
+    return;
+  };
+  return (
+    <Stack spacing={3} mx={1} my={5}>
+      <Stack spacing={3} direction={'row'}>
+        <TextField
+          id="cgpa"
+          label="CGPA"
+          value={cgpa}
+          onChange={(e) => {
+            setCgpa(e.target.value);
+          }}
+          variant="outlined"
+        />
+      </Stack>
+      <Button
+        variant="outlined"
+        type="submit"
+        sx={{
+          mt: 1,
+          boxShadow: 2,
+        }}
+        onClick={handleClick}
+      >
+        Calculate
+      </Button>
+
+      {result.length !== 0 && (
+        <Typography
+          variant="h6"
+          sx={{
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
+        >
+          {result}
+        </Typography>
+      )}
+
+      {error.length !== 0 && (
+        <Typography
+          variant="h6"
+          sx={{
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
+          //  style={{ color: theme.palette.error.main }}
+        >
+          {error}
+        </Typography>
+      )}
+    </Stack>
+  );
 };
 
 export default CgpaToPercentage;
